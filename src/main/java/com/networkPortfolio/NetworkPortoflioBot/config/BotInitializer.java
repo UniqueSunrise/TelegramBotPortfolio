bbe22cd9eacef1dd976fca7677c78d57d8a9fcef
UniqueSunrise <renatsigma@gmail.com>
2022-11-14 20:50:36 +0500

    Added te;legram bot menu

--- src/main/java/com/networkPortfolio/NetworkPortoflioBot/config/BotInitializer.java
+++ src/main/java/com/networkPortfolio/NetworkPortoflioBot/config/BotInitializer.java
@@ -9,6 +9,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+
 @Component
 public class BotInitializer {
 
--- src/main/java/com/networkPortfolio/NetworkPortoflioBot/service/TelegramBot.java
+++ src/main/java/com/networkPortfolio/NetworkPortoflioBot/service/TelegramBot.java
@@ -4,9 +4,16 @@ package com.networkPortfolio.NetworkPortoflioBot.service;
 import com.networkPortfolio.NetworkPortoflioBot.config.BotConfig;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
@@ -15,6 +22,17 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     public TelegramBot(BotConfig config) {
         this.config = config;
+        List<BotCommand> listofCommands = new ArrayList<>();
+        listofCommands.add(new BotCommand("/start", "welcome message"));
+        listofCommands.add(new BotCommand("/add_portfolio", "add your portfolio on dropstab"));
+        listofCommands.add(new BotCommand("/delete_portfolio", "delete your portfolio"));
+        listofCommands.add(new BotCommand("/information", "information how to use this telegram bot"));
+        try {
+            this.execute(new SetMyCommands(listofCommands, new BotCommandScopeDefault(), null));
+        }
+        catch (TelegramApiException e) {
+
+        }
     }
 
     @Override
@@ -37,11 +55,22 @@ public class TelegramBot extends TelegramLongPollingBot {
 
             switch (messageText) {
                 case "/start":
-
                         startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                         break;
-                default:
 
+                case "/add_portfolio":
+                        sendMessage(chatId, "In progress");
+                        break;
+                case "/delete_portfolio":
+                        sendMessage(chatId, "In progress");
+                        break;
+                case "/information":
+                        sendMessage(chatId, "In progress");
+                        break;
+
+
+
+                default:
                         sendMessage(chatId, "Sorry, command was not recognized");
 
             }
